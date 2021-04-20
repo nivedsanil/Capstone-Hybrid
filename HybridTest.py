94dde9530ddb07644c09c4862120754de16b9aff
nivedsanil <nivedsanil@hotmail.com>
2021-04-20 21:45:22 +0530

    Made the code working

--- HybridTest.py
+++ HybridTest.py
@@ -22,16 +22,19 @@ def LoadData():
 
 evaluator = Evaluator(data_evaluation, ranks)
 
-print("Computing content-based similarity based on Genre, Year and Mise En Scene similarity")
+
 ContentBased = ContentFiltering()
 
-print("Computing Item-Based Collaborative Filtering")
+
 itemKNN = KNNBasic(sim_options = {'name': 'cosine', 'user_based': False}, k=15) 
 
 Hybrid = HybridAlgorithm([ContentBased,itemKNN], [0.75,0.25])
 
+# print("Computing content-based similarity based on Genre, Year and Mise En Scene similarity")
 evaluator.AddAlgorithm(ContentBased, "Content Based Filtering")
+
 evaluator.AddAlgorithm(itemKNN, "Item-Based Collaborative Filtering")
+
 evaluator.AddAlgorithm(Hybrid, "Hybrid")
 
 evaluator.Evaluate(True)
